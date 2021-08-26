09b16b22bcf06d6a26d0f6706ff7bde7e3cff8ec
kamesh28 <sanyasiraomukkavalli@gmail.com>
2021-08-26 12:11:08 +0530

    Smart Registration Project

---
+++ src/main/java/com/smart/SmartregistrationApplication.java
@@ -0,0 +1,15 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartregistrationApplication.class, args);
+	}
+
+	
+	
+}
---
+++ src/main/java/com/smart/helper/Message.java
@@ -0,0 +1,32 @@
+package com.smart.helper;
+
+public class Message {
+	
+	private String contents;
+	private String type;
+	
+	public Message(String contents, String type) {
+		super();
+		this.contents = contents;
+		this.type = type;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	
+
+}
